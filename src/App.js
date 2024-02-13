adffb0d5105ec701cbaaab85e4195657ff131d1e
Bandini2001 <bandinikohare30@gmail.com>
2024-02-13 22:10:55 +0530

    add concept and data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,173 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import moment from 'moment-timezone';
+import Config from "./util.json";
 
-function App() {
+const App = () => {
+
+  const [selectedWeek, setSelectedWeek] = useState(moment());
+  const [selectedTimezone, setSelectedTimezone] = useState('UTC');
+  const [weeklySchedule, setWeeklySchedule] = useState([]);
+
+  // Load weekly data when the selected week or timezone changes
+  useEffect(() => {
+    const startOfWeek = selectedWeek.clone().startOf('week');
+    const endOfWeek = selectedWeek.clone().endOf('week');
+
+    // Dummy function to load weekly working days and times
+    const loadWeeklyData = () => {
+      const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+      const times = Array.from({ length: 1 }, (_, i) => i + 1); // 8 AM to 11 PM
+      const schedule = days.map(day => ({
+        day,
+        times: times.map(time => ({
+          hour: `${time < 10 ? '0' : ''}${time}:00`,
+          checked: false // Initial checkbox state
+        }))
+      }));
+      setWeeklySchedule(schedule);
+    };
+
+    loadWeeklyData();
+  }, [selectedWeek, selectedTimezone]);
+
+  // Move to the previous or next week
+  const handlePrevWeek = () => {
+    setSelectedWeek(selectedWeek.clone().subtract(1, 'week'));
+  };
+
+  const handleNextWeek = () => {
+    setSelectedWeek(selectedWeek.clone().add(1, 'week'));
+  };
+
+  // Handle timezone change
+  const handleTimezoneChange = (event) => {
+    setSelectedTimezone(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='parent-division'>
+        <div className='header-div'>
+
+          <div> <p className='button' onClick={handlePrevWeek}>Previous Week</p></div>
+
+          <div><p>{selectedWeek.startOf('week').format('MMMM Do YYYY')} - {selectedWeek.endOf('week').format('MMMM Do YYYY')}</p></div>
+
+          <div> <p className='button' onClick={handleNextWeek}>Next Week</p>
+          </div>
+
+
+        </div>
+        Timezone:<br />
+        <select value={selectedTimezone} onChange={handleTimezoneChange} className='timezone'>
+          <option value="UTC">UTC</option>
+          <option value="America/New_York">America/New York</option>
+          {/* Add more timezones as needed */}
+        </select>
+        <div>
+          Weekly Schedule:
+          <table>
+            <thead>
+              <tr>
+                <th>Day</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              {
+                weeklySchedule.map((day, index) => (
+                  <tr key={index}>
+                    <td>{day.day}</td>
+                    {day.times.map((timeSlot, index) => (
+                      <td key={index}>
+
+                        
+                          
+
+                          {
+                            Config.days.map((tim, index) => {
+                              const { time1, time2, t3, t4,t5,t6,t7,t8,t9,t10,t11} = tim
+                              return (
+                                <div className='d-flex'>
+                                  <div className='card-container' key={index}>
+                                    <div className='card-1'>
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{time1}</p>
+                                    </div>
+
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{time2}</p>
+                                    </div>
+
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{t3}</p>
+                                    </div>
+
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{t4}</p>
+                                    </div>
+
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{t5}</p>
+                                    </div>
+
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{t6}</p>
+                                    </div>
+
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{t7}</p>
+                                    </div>
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{t8}</p>
+                                    </div>
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{t9}</p>
+                                    </div>
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{t10}</p>
+                                    </div>
+                                    <div className='day'>
+                                    <input type="checkbox" checked={timeSlot.checked} onChange={() => { }} /> 
+                                    <p>{t11}</p>
+                                    </div>
+                                   
+                                    
+                                      
+                                    </div>
+                                  </div>
+                                </div>
+                              )
+                            })
+                          }
+                         
+                          {/* {
+                 Array.from({ length: 16  }, (_, i) => i + 8).map(time => (
+             <th key={time}>{`${time < 10 ? '0' : ''}${time}:00 ${time}:30`}</th>
+           ))} */}
+                         <hr/>
+                      </td>
+                     
+                    ))}
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
